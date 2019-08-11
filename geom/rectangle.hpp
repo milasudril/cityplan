b981c6ef28e5a1f8a194d3dc7729687dffc46292
Torbjörn Rathsman <milasudril@gmail.com>
2019-08-11 08:13:26 +0200

    Reduce the number of member functions on rectangle

--- geom/rectangle.hpp
+++ geom/rectangle.hpp
@@ -21,50 +21,12 @@ namespace Cityplan
 
 			explicit Rectangle(Position origin, Dimension dim):m_origin{origin}, m_dim{dim}{}
 
-			Coordinate x0() const
-				{return m_origin.x();}
-
-			Rectangle& x0(Coordinate c)
-				{
-				m_origin.x(c);
-				return *this;
-				}
-
-			Coordinate y0() const
-				{return m_origin.y();}
-
-			Rectangle& y0(Coordinate c)
-				{
-				m_origin.y(c);
-				return *this;
-				}
-
 			Position position() const
 				{return m_origin;}
 
 			Rectangle& position(Position origin)
 				{
 				m_origin = origin;
-				return *this;
-				}
-
-
-
-			Length width() const
-				{return m_dim.width();}
-
-			Rectangle& width(Length l)
-				{
-				m_dim.width(l);
-				return *this;
-				}
-
-			Length height() const
-				{return m_dim.height();}
-
-			Rectangle& height(Length l)
-				{
-				m_dim.height(l);
 				return *this;
 				}
 
