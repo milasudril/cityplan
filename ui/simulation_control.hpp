d1d1aed7a180b9e6ce6e7aae7818d1a2fdb76a17
Torbjörn Rathsman <milasudril@gmail.com>
2019-08-11 15:38:23 +0200

    Initial work on UI

---
+++ ui/simulation_control.hpp
@@ -0,0 +1,30 @@
+//@	{"targets":[{"name":"simulation_control.hpp","type":"include"}]}
+
+#ifndef CITYPLAY_SIMULATION_CONTROL_HPP
+#define CITYPLAY_SIMULATION_CONTROL_HPP
+
+#include "./ui_container.hpp"
+#include "./ui_box.hpp"
+#include "./ui_button.hpp"
+
+#include "model/simulation.hpp"
+
+namespace Cityplan
+	{
+	class SimulationControl
+		{
+		public:
+			explicit SimulationControl(Simulation& sim, UiContainer& container, UiBoxOrientation orientation);
+
+		private:
+			UiBox m_box;
+			UiButton m_load_state;
+			UiButton m_discard;
+			UiButton m_save;
+
+			Simulation& r_sim;
+		};
+
+	}
+
+#endif
