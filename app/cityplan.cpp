33421b1e2f70a3982014e0f4b06e034cec9c7d4e
Torbjörn Rathsman <milasudril@gmail.com>
2019-08-11 17:53:59 +0200

    Move toolbar into vertical box

--- app/cityplan.cpp
+++ app/cityplan.cpp
@@ -2,6 +2,7 @@
 
 #include "ui/ui_window.hpp"
 #include "ui/simulation_control.hpp"
+#include "ui/ui_box.hpp"
 
 #include <gtk/gtk.h>
 
@@ -9,8 +10,9 @@ int main(int argc, char* argv[])
 	{
 	gtk_init(&argc, &argv);
 	Cityplan::UiWindow mainwin{"Cityplan"};
+	Cityplan::UiBox box{mainwin, Cityplan::UiBox::Orientation::Horizontal};
 	Cityplan::Simulation sim{Cityplan::Dimension{}.width(20).height(20)};
-	Cityplan::SimulationControl sim_ctrl{sim, mainwin, Cityplan::UiBox::Orientation::Horizontal};
+	Cityplan::SimulationControl sim_ctrl{sim, box, Cityplan::UiBox::Orientation::Horizontal};
 	mainwin.callback<0>(sim_ctrl);
 	mainwin.show();
 	gtk_main();
